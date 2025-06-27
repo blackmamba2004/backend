5a9fa2e0fd797008a02b9274bb8b28f6edf3b792
blackmamba2004 <vasilsemen04@gmail.com>
2025-06-27 19:27:23 +0300

    Revert "fix: users/register brokers/register"
    This reverts commit a013b84da0d5396c5a2a695b75802fb975618f71.

--- app/components/decorators/unique.py
+++ app/components/decorators/unique.py
@@ -1,6 +1,6 @@
 import functools
 import logging
-from typing import Callable, TypeVar, Awaitable, Type
+from typing import Callable, TypeVar, Awaitable
 
 from sqlalchemy.exc import IntegrityError
 
@@ -12,30 +12,31 @@ F = TypeVar('F', bound=Callable[..., Awaitable])
 logger = logging.getLogger(__name__)
 
 
-def unique_error(error_class: Type[ApplicationException]):
+def unique_error(func: F) -> F:
     """
-    Параметризованный декоратор: ловит ошибки уникальности и поднимает переданный тип исключения
+    Декоратор для отлова ошибки нарушения уникальности столбца в базе данных
     """
-    def decorator(func: F) -> F:
-        @functools.wraps(func)
-        async def wrapper(*args, **kwargs) -> Awaitable:
-            try:
-                return await func(*args, **kwargs)
-            except IntegrityError as e:
-                logger.debug(f"IntegrityError catched: {e}")
-                error_message = str(e)
 
-                if "duplicate key value violates unique" in error_message:
-                    if 'Key (' in error_message:
-                        field_name = error_message.split('Key (')[1].split(')')[0]
-                        final_message = f"This {field_name} is taken"
-                    
-                    raise error_class(message=final_message) from e
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs) -> Awaitable:
+        err_obj_template: ApplicationException = kwargs.pop("error")
+        try:
+            return await func(*args, **kwargs)
+        except IntegrityError as e:
+            logger.debug(f"IntegrityError catched: {e}")
+            error_message = str(e)
+
+            if "duplicate key value violates unique" in error_message:
+                final_message = "unknown"
+                field_name = None
+                if 'Key (' in error_message:
+                    field_name = error_message.split('Key (')[1].split(')')[0]
+                    final_message = f"This {field_name} is taken"
                 
-                else:
-                    logger.error(error_message, exc_info=e)
-                    raise
+                raise err_obj_template(message=final_message) from e
+            
+            else:
+                logger.error(error_message, exc_info=e)
+                raise
 
-        return wrapper
-    
-    return decorator
+    return wrapper
